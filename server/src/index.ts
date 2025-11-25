6b6c724ca205c3389996537c79acfc82c226fd1d
Brandon Ebersohl <brandon.ebersohl@gmail.com>
2025-11-25 14:31:51 -0700

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
   const clientDistPath = join(__dirname, '../../client/dist/client/browser');
   app.use(express.static(clientDistPath));
 
-  app.get('*', (req: Request, res: Response) => {
+  app.get(/(.*)/, (req: Request, res: Response) => {
     res.sendFile(join(clientDistPath, 'index.html'));
   });
 }
